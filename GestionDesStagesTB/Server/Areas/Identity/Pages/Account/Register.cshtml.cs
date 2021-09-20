7d0a8a2d9dbcc5bae5794a9b9133e211eda04f5c
ThierryBab <thierry.babault.20@hotmail.fr>
2021-09-20 14:25:42 -0400

    1st commit

--- GestionDesStagesTB/Server/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ GestionDesStagesTB/Server/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -82,6 +82,7 @@ namespace GestionDesStagesTB.Server.Areas.Identity.Pages.Account
                 var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
+
                 {
                     _logger.LogInformation("User created a new account with password.");
 
